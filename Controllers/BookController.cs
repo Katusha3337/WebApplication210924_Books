3332b9103da9247ea36bf590edeec0726c8814b4
Marka_rb95@student.itstep.org <Marka_rb95@student.itstep.org>
2024-09-29 22:07:49 +0300

    Добавьте файлы проекта.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication210924_Books.Models;
+
+namespace WebApplication210924_Books.Controllers
+{
+    public class BooksController : Controller
+    {
+        private static List<Book> books = new List<Book>
+    {
+        new Book { Id = 1, Title = "Книга 1", Price = 100, Description = "Описание книги 1", ImageUrl = "/images/book1.jpeg" },
+        new Book { Id = 2, Title = "Книга 2", Price = 200, Description = "Описание книги 2", ImageUrl = "/images/book2.jpeg" }
+    };
+
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+
+        public IActionResult Order(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Order(int id, string name, string phone, string city, string postOffice)
+        {
+            
+            ViewBag.Message = "Спасибо за ваш заказ!";
+            return View("ThankYou");
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication210924_Books.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
